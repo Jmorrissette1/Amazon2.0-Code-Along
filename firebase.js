9ad4d7b5e3a9ed871437293e9ef425c680476ad5
Jmorrissette1 <J.morrissette47@gmail.com>
2021-07-30 17:04:28 -0700

    Built out card page, and added firebase connection.

---
+++ firebase.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCnWbffi76M8y2dWIc8j-nq2egBw04-Y2c",
+    authDomain: "clone-two-60465.firebaseapp.com",
+    projectId: "clone-two-60465",
+    storageBucket: "clone-two-60465.appspot.com",
+    messagingSenderId: "449167013508",
+    appId: "1:449167013508:web:ff4215d9059cba1b06b4d1",
+    measurementId: "G-F018GFYWN3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
