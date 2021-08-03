a050f0286f4e8f2869664443f29d8bbe227be888
Jmorrissette1 <47222019+Jmorrissette1@users.noreply.github.com>
2021-08-02 19:06:30 -0700

    Pulling Data from Database, Added add to cart button

--- app.js
+++ app.js
@@ -11,36 +11,49 @@ function getItems(){
               productMake : doc.data().productMake,
               rating: doc.data().rating,
               price: doc.data().price
+          })
         
-          });
         });
-       // generateItems(items);
-        console.log(items);
+        generateItems(items);
+      
     });
  
   
 }
+function addToCart(item){
+    console.log("Add TO Cart Click");
+    console.log(item);
+    
+}
 
 
 function generateItems(items){
-    let itemsHTML ="";
+   
     items.forEach((item) => {
         let doc = document.createElement("div");
-        doc.classList.add("main-product", "mr-5");
-      
-        itemsHTML += ` <div class="main-product">
+        doc.classList.add("main-product", "mr-7");
+        doc.innerHTML= `
         <div class="product-image w-48 h-42 bg-white">
             <img class="w-full h-full object-contain p-4" src="${item.image}" alt="Pins">
         </div>
-        <div class="product-name text-black font-bold mt-2 text-sm">${item.productName}/div>
+        <div class="product-name text-black font-bold mt-2 text-sm">${item.productName}</div>
         <div class="product-make">${item.productMake}</div>
-        <div class="product-rating my-1 text-black">⭐⭐⭐⭐⭐ ${item.raiting}</div>
+        <div class="product-rating my-1 text-black">⭐⭐⭐⭐⭐ ${item.rating}</div>
         <div class="product-price text-lg font-bold">${item.price}</div>
-        <button class="add-to-cart-btn">add to cart</button>
-        </div>  `
+        
+        `
+      
+       let addToCartEl = document.createElement("div");
+       addToCartEl.classList.add("add-to-cart-btn");
+       addToCartEl.innerText ="add to cart";
+       addToCartEl.addEventListener("click", function(){
+           addToCart(item);
+       });
+       doc.appendChild(addToCartEl);
+       document.querySelector(".main-section-products").appendChild(doc);
     });
-
-    document.querySelector("main-section-products").innerHTML =itemsHTML;
+    
+   
 }
 
 getItems();
